ebe1ca2b86cc53dc6229ab2bff1fa60d1e1fa93e
Alexsusus <117087891+Alexsusus@users.noreply.github.com>
2023-10-08 18:45:44 +0300

    FragmentMoviesDetails added coroutines

--- app/src/main/java/com/example/movie/FragmentMoviesDetails.kt
+++ app/src/main/java/com/example/movie/FragmentMoviesDetails.kt
@@ -2,7 +2,6 @@ package com.example.movie
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,7 +9,6 @@ import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -18,13 +16,16 @@ import com.example.movie.adapter.ActorsAdapter
 import com.example.movie.repository.ActorRepository
 import com.example.movie.repository.GenreRepository
 import com.example.movie.repository.MovieRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class FragmentMoviesDetails : Fragment() {
 
     private var someFragmentClickListener: SomeFragmentClickListener? = null
     private lateinit var list: RecyclerView
-    private val TAG = "TAG"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,58 +41,70 @@ class FragmentMoviesDetails : Fragment() {
         val storyLine = view.findViewById<TextView>(R.id.text_storyline)
 
 
-        var movieId = arguments?.getInt("id_movie")
+        val movieId = arguments?.getInt("id_movie")
         val movieRepository = MovieRepository()
-        val movie = movieId?.let { context?.let { it1 -> movieRepository.findMovieById(it1, it) } }
-        if (movie != null) {
-            Glide.with(imageBackground.context)
-                .load(movie.background)
-                .into(imageBackground)
-
-            if (movie.isAdult) {
-                ageLimit.visibility = View.VISIBLE
-            } else {
-                ageLimit.visibility = View.GONE
-            }
-
-            title.text = movie.title
-
-            val genreRepository = context?.let { GenreRepository(it) }
-            val genreIds = movie.genreIds ?: emptyList()
-            val genreNames = genreRepository?.findGenreNamesByIds(genreIds)
-            if (genreNames != null) {
-                genres.text = genreNames.joinToString(", ")
-            }
-            reviews.text = movie.reviews.toString()
-            storyLine.text = movie.storyline
-        }
         val actorRepository = context?.let { ActorRepository(it) }
 
-        if (movie != null) {
-            Log.d(TAG, "onCreateView: ${movie.actors}")
-        }
+        val uiScope = CoroutineScope(Dispatchers.Main)
+        uiScope.launch {
 
-        val actors = movie?.actors?.let { actorRepository?.findActorsByMovieId(it) }
-        val layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-        if (actors != null) {
-            layoutManager.initialPrefetchItemCount = actors.size
-        }
-        list.layoutManager = layoutManager
-        val adapter = context?.let {
-            actors?.let { it1 -> ActorsAdapter(it, it1) }
-        }
-
-        list.adapter = adapter
-
-        view.findViewById<ImageButton>(R.id.backButton).apply {
-            setOnClickListener {
-                someFragmentClickListener?.backToMoviesList()
+            val movie = withContext(Dispatchers.IO) {
+                movieId?.let { context?.let { it1 -> movieRepository.findMovieById(it1, it) } }
             }
-        }
 
-        savedInstanceState?.let {
-            val adapterState = it.getBundle("adapter_state")
-            layoutManager.onRestoreInstanceState(adapterState)
+            val actors = withContext(Dispatchers.IO) {
+                movie?.actors?.let {
+                    actorRepository?.findActorsByMovieId(it)
+                }
+            }
+
+            if (movie != null) {
+                Glide.with(imageBackground.context)
+                    .load(movie.background)
+                    .into(imageBackground)
+
+                if (movie.isAdult) {
+                    ageLimit.visibility = View.VISIBLE
+                } else {
+                    ageLimit.visibility = View.GONE
+                }
+
+                title.text = movie.title
+
+                val genreRepository = context?.let { GenreRepository(it) }
+                val genreIds = movie.genreIds ?: emptyList()
+
+                val genreNames =
+                    withContext(Dispatchers.IO) { genreRepository?.findGenreNamesByIds(genreIds) }
+
+                if (genreNames != null) {
+                    genres.text = genreNames.joinToString(", ")
+                }
+                reviews.text = movie.reviews.toString()
+                storyLine.text = movie.storyline
+            }
+
+            val layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+            if (actors != null) {
+                layoutManager.initialPrefetchItemCount = actors.size
+            }
+            list.layoutManager = layoutManager
+            val adapter = context?.let {
+                actors?.let { it1 -> ActorsAdapter(it, it1) }
+            }
+
+            list.adapter = adapter
+
+            view.findViewById<ImageButton>(R.id.backButton).apply {
+                setOnClickListener {
+                    someFragmentClickListener?.backToMoviesList()
+                }
+            }
+
+            savedInstanceState?.let {
+                val adapterState = it.getBundle("adapter_state")
+                layoutManager.onRestoreInstanceState(adapterState)
+            }
         }
 
         return view
